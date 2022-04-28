ef645e02e3d6ddda6b9d5d036e8861907a52174c
MattRees-Dev <matthewrees1992@gmail.com>
2022-04-28 16:29:25 +0100

    Update

--- Testimonials Slider/js/app.js
+++ Testimonials Slider/js/app.js
@@ -1,6 +1,24 @@
 'use strict';
 
-const arrowLeft = document.querySelector('.arrow--left');
-const arrowRight = document.querySelector('.arrow--right');
+const quoteOne = document.querySelector('.quote-one');
+const quoteTwo = document.querySelector('.quote-two');
+const portraitOne = document.querySelector('.portrait--one');
+const portraitTwo = document.querySelector('.portrait--two');
+const prevBtn = document.querySelector('.arrow--left');
+const nextBtn = document.querySelector('.arrow--right');
 
-// const carouselToggle = function() { }
+prevBtn.addEventListener('click', function () {
+    console.log('Prev');
+    quoteOne.classList.toggle('hidden');
+    quoteTwo.classList.toggle('hidden');
+    portraitOne.classList.toggle('hidden');
+    portraitTwo.classList.toggle('hidden');
+});
+
+nextBtn.addEventListener('click', function () {
+    console.log('Next');
+    quoteOne.classList.toggle('hidden');
+    quoteTwo.classList.toggle('hidden');
+    portraitOne.classList.toggle('hidden');
+    portraitTwo.classList.toggle('hidden');
+});
