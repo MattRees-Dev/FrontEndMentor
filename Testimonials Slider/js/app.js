aec4b5743649227d6f156819726e16cdcc9c41b7
MattRees-Dev <matthewrees1992@gmail.com>
2022-04-29 12:30:36 +0100

    Update

--- Testimonials Slider/js/app.js
+++ Testimonials Slider/js/app.js
@@ -7,12 +7,21 @@ const portraitTwo = document.querySelector('.portrait--two');
 const prevBtn = document.querySelector('.arrow--left');
 const nextBtn = document.querySelector('.arrow--right');
 
+// Media Query
+const mediaQuery = window.matchMedia('(max-width: 375px)');
+const user = document.querySelector('.user');
+if (mediaQuery.matches) {
+    console.log('This Works!!!');
+    user.classList.toggle('column');
+    user.classList.toggle('row');
+}
+
 prevBtn.addEventListener('click', function () {
     console.log('Prev');
     quoteOne.classList.toggle('hidden');
     quoteTwo.classList.toggle('hidden');
     portraitOne.classList.toggle('hidden');
-    portraitTwo.classList.toggle('hidden'); 
+    portraitTwo.classList.toggle('hidden');
 });
 
 nextBtn.addEventListener('click', function () {
@@ -22,3 +31,4 @@ nextBtn.addEventListener('click', function () {
     portraitOne.classList.toggle('hidden');
     portraitTwo.classList.toggle('hidden');
 });
+
